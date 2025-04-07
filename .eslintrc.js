16225c58b3b19b5fe059310d7b7a0a695b15916f
Nataliia Kyrychenko <nataliia_kyrychenko@epam.com>
2025-04-07 17:34:11 +0300

    Debug eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-   plugins: ["security-node"],
-   extends: ["plugin:security-node/recommended"],
-   parserOptions: {
-     ecmaVersion: 2020
-     sourceType: "module" // <-- Добавь вот это
-   },
-   env: {
-     es6: true,
-     node: true
-   }
- };
+  plugins: ["security-node"],
+  extends: ["plugin:security-node/recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  env: {
+    es6: true,
+    node: true
+  }
+};
